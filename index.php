cf5845692b0b54a8589e90aeae93bbd2f39d378c
Ajay M V <ajaymv@ajaymv.com>
2019-07-20 19:22:52 +0530

    Changed index page contents2.

--- index.php
+++ index.php
@@ -1,8 +1,8 @@
 <html>
  <head>
-  <title>PHP Test</title>
+  <title>PHP Test 2</title>
  </head>
  <body>
- <?php echo '<p>Hello World</p>'; ?> 
+ <?php echo '<p>Hello World Again</p>'; ?> 
  </body>
 </html>
