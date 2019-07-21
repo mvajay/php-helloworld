6602b519e8a8391d1b203c56ceabf344bd8a6d69
Ajay M V <ajaymv@ajaymv.com>
2019-07-21 20:54:50 +0530

    updated app 5

--- index.php
+++ index.php
@@ -3,7 +3,8 @@
   <title>PHP Test 5</title>
  </head>
  <body>
- <?php echo '<p>Hello World Again 5</p>'; ?> 
+ <?php echo '<p>Hello World Again 5</p>'; 
 	print "Hello, World Again 5! php version is " . PHP_VERSION . "\n";
+?>
  </body>
 </html>
