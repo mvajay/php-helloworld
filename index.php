0420b642096fbed37dd4d96ad93d612b8bc46b01
Ajay M V <ajaymv@ajaymv.com>
2019-08-11 22:59:29 +0530

    changed index.php 7th time

--- index.php
+++ index.php
@@ -1,10 +1,10 @@
 <html>
  <head>
-  <title>PHP Test 6</title>
+  <title>PHP Test 7</title>
  </head>
  <body>
- <?php echo '<p>Hello World Again 6</p>'; 
-	print "Hello, World Again 6! php version is " . PHP_VERSION . "\n";
+ <?php echo '<p>Hello World Again 7</p>'; 
+	print "Hello, World Again 7! php version is " . PHP_VERSION . "\n";
 ?>
  </body>
 </html>
